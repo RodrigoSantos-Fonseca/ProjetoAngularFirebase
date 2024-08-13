e4eb0562d5fc7949a9cb6b45bc91127656aa3883
Rodrigo Santos Fonseca <162513597+RodrigoSantos-Fonseca@users.noreply.github.com>
2024-08-13 22:33:07 +0000

    curriculo

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -11,5 +11,38 @@ import { MessageService } from '../services/message.service';
 })
 export class HomePage {
 
+  pessoa ={
+    foto: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQAv4YmLgkzgbI3sdzubVgtNTRxRFUpH0O25UcaQDHhwgzOVPuOWehjPfQlib-IperHP9ltdHa5Hwl37piXU1jyFxNTufbG73bnFflikw',
+    nome: 'Chefinho',
+    objetivo: 'Programador HTML & CSS',
+    contato:{
+      email: 'chefinho@gmail.com',
+      telefone: '(11) 991696969',
+      github: 'github.com/chefinho',
+      linkedin:'linkedin.com/chefinho'
+    },
+    softskills:[
+      'Trabalho em Grupo',
+      'Comunicação',
+      'Resiliência',
+      'Proatividade',
+      'Criatividade'
+    ],
+    formacao:[
+      {
+        ano_inicio: '2023',
+        ano_termino: '2024',
+        instituicao: 'Etec Sales Gomes',
+        curso: 'Desenvovilmento de Sistemas'
+      },
+      {
+        ano_inicio: '2025',
+        ano_termino: '2028',
+        instituicao: 'Faculdade de Tecnologia de Tatuí',
+        curso: 'Análise e Desenvovilmento de Sistemas'
+      }
+    ]
+  }
+
   constructor(){ }
 }
