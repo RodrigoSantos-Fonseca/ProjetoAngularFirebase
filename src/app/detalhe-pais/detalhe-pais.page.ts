7c44735019b067759d6c9671c7d79e49f7efb603
Rodrigo Santos Fonseca <162513597+RodrigoSantos-Fonseca@users.noreply.github.com>
2024-09-07 00:30:33 +0000

    site paises

---
+++ src/app/detalhe-pais/detalhe-pais.page.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-detalhe-pais',
+  templateUrl: './detalhe-pais.page.html',
+  styleUrls: ['./detalhe-pais.page.scss'],
+})
+export class DetalhePaisPage implements OnInit {
+
+
+  codigo: any;
+  paises: any;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.codigo = history.state.codigo;
+    fetch(`https://restcountries.com/v3.1/alpha/${this.codigo}`)
+    .then(dados => dados.json())
+    .then(dados => {
+      console.log(dados);
+      this.paises = dados;
+    })
+    .catch(erro => { console.log(erro); })
+    .finally(() => {console.log('finalizado!');})
+  }
+
+}
