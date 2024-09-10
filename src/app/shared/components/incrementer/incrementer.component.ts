bbd068a36266b7d8c254f63dd3c1771f8c16084a
Rodrigo Santos Fonseca <162513597+RodrigoSantos-Fonseca@users.noreply.github.com>
2024-09-10 22:46:14 +0000

    contador

---
+++ src/app/shared/components/incrementer/incrementer.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { IncrementerService } from 'src/app/services/incrementer.service';
+
+@Component({
+  selector: 'app-incrementer',
+  templateUrl: './incrementer.component.html',
+  styleUrls: ['./incrementer.component.scss'],
+})
+export class IncrementerComponent  {
+
+
+  constructor(
+    public incrementService: IncrementerService
+  ) { }
+
+  decrement() {
+    if (this.incrementService.valor > 0)
+    this.incrementService.valor--;
+  }
+
+  increment() {
+    this.incrementService.valor++;
+  }
+}
